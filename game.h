0d559075f2e4df486a7eced76fb49d6f7c14aa64
siru <sirius65535@gmail.com>
2022-04-28 23:14:40 +0800

    change to c fine

--- game.h
+++ game.h
@@ -20,28 +20,3 @@ void game_init(void);
 void handle_actions(void);
 void update_game(float time);
 NodePlayer* query_has(int fd);
-
-#ifdef _GAME_H_IMPLEMENTATION_
-
-static Game game;
-void game_init(void) {
-    action_queue_init(&game.aq);
-    game.ll_player.head = NULL;
-    game.ll_platform.head = NULL;
-    game.state = Starting;
-    str_with_mutex_init(&game.environment);
-}
-
-void handle_actions(void) {
-    assert(false && "handle_actions is not implemented yet");
-}
-
-void update_game(float time) {
-    assert(false && "update_game is not implemented yet");
-}
-
-NodePlayer* query_has(int fd) {
-    return _query_has(&game.ll_player, fd);
-}
-
-#endif
