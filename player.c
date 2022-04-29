e0af19bf3d20d486c00b56f5ad2a5535b360e8b1
siru <sirius65535@gmail.com>
2022-04-29 14:06:06 +0800

    dir and action implementation

--- player.c
+++ player.c
@@ -1,5 +1,12 @@
 #include "player.h"
 
+Dir get_dir(char c)
+{
+    if (c == '1') return Left;
+    if (c == '2') return Right;
+    return NoDir;
+}
+
 NodePlayer* _query_has(LinkedListPlayer* ll_player, int fd)
 {
     if (ll_player == NULL)
@@ -13,4 +20,40 @@ NodePlayer* _query_has(LinkedListPlayer* ll_player, int fd)
         tmp = tmp->next;
     }
     return NULL;
+}
+
+void create_player(LinkedListPlayer* ll_player, Player player)
+{
+    NodePlayer* node = (NodePlayer*)malloc(sizeof(NodePlayer));
+    node->p = player;
+    // node->p = (Player) {
+    //     .rect = rect_origin(),
+    //     .heart = 100.0f,
+    //     .name = name,
+    //     .dir = NoDir,
+    //     .fd = fd,
+    // };
+
+    node->next = ll_player->head;
+    ll_player->head->prev = node;
+    ll_player->head = node;
+}
+
+void delete_player(LinkedListPlayer* ll_player, NodePlayer* node)
+{
+    if (node == NULL) return;
+
+    NodePlayer *front = node->prev;
+    NodePlayer *back = node->next;
+    if (front) front->next = back;
+    if (back) back->prev = front;
+
+    free(node);
+}
+
+void change_player_dir(LinkedListPlayer* ll_player, NodePlayer* node, Dir dir)
+{
+    if (node == NULL) return;
+
+    node->p.dir = dir;
 }
