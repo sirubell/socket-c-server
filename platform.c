d9faf748cefdd5e8596a90756a88db830ff4e61d
siru <sirius65535@gmail.com>
2022-04-29 14:23:32 +0800

    implement platform

---
+++ platform.c
@@ -0,0 +1,42 @@
+#include <stdlib.h>
+
+#include "platform.h"
+
+void create_platform(LinkedListPlatform* ll_platform, Platform platform)
+{
+    NodePlatform* node = (NodePlatform*)malloc(sizeof(NodePlatform));
+    node->p = platform;
+    // node->p = (Player) {
+    //     .rect = rect_origin(),
+    //     .heart = 100.0f,
+    //     .name = name,
+    //     .dir = NoDir,
+    //     .fd = fd,
+    // };
+
+    node->next = ll_platform->head;
+    ll_platform->head->prev = node;
+    ll_platform->head = node;
+}
+
+void delete_platform(LinkedListPlatform* ll_platform, NodePlatform* node)
+{
+    if (node == NULL) return;
+
+    NodePlatform *front = node->prev;
+    NodePlatform *back = node->next;
+    if (front) front->next = back;
+    if (back) back->prev = front;
+
+    free(node);
+}
+
+Rect platform_random(void)
+{
+    return (Rect) {
+        .x = 300,
+        .y = 900,
+        .w = 100,
+        .h = 20,
+    };
+}
