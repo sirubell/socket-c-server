cab27cceb36938c620bc61a78427e9720933f138
siru <sirius65535@gmail.com>
2022-04-29 14:06:23 +0800

    action function

--- platform.h
+++ platform.h
@@ -24,4 +24,7 @@ typedef struct {
     NodePlatform* head;
 } LinkedListPlatform;
 
+void create_platform(LinkedListPlatform* ll_platform, Platform platform);
+void delete_platform(LinkedListPlatform* ll_platform, NodePlatform* node);
+
 #endif
